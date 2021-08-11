f62f4dfae42ceac36d5785180819e69046a6a8ec
sunf044 <longhai044207@gmail.com>
2021-08-12 00:17:49 +0700

    version 1

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'test',
+              'version 1',
             ),
             Text(
               '$_counter',
