6e7554e76a870cd3cfaca50071cb265dbd30e9d9
sunf044 <longhai044207@gmail.com>
2021-08-12 19:15:59 +0700

    it work! v2

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'it work! v1',
+              'it work! v2',
             ),
             Text(
               '$_counter',
